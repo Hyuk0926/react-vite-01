d3e92a235adf3f5ba33b1593cd120a4a37fe08ef
hyuk <chldmsgur09@gmail.com>
2025-04-21 13:16:05 +0900

    컴포넌트 생성 실습

---
+++ src/component.tsx
@@ -0,0 +1,12 @@
+const Component = () => {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>리액트는 UI를 만드는 라이브러리입니다.</p>
+        </div>
+    );
+}
+
+export default Component;  //export default를 선언해야 다른 파일에서 import할 수 있다.
+
+
